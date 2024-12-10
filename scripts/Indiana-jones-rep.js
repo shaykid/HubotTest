ddf444736450d4cdc02f891794112745dd356264
Shay Avraham <github@hinbit.com>
2024-12-10 13:02:41 +0200

    Update Indiana-jones-rep.js

--- scripts/Indiana-jones-rep.js
+++ scripts/Indiana-jones-rep.js
@@ -1,13 +1,13 @@
-module.exports = (robot) => {
-  robot.respond(/hey|hello|hi/i, (res) => {
-    res.send('Howdy, partner! This is Indy. Ready to explore the red planet?');
+module.exports = function (robot) {
+  robot.respond(/(?:hey|hello|hi)/i, function (res) {
+    res.send("Howdy, partner! This is Indy. Ready to explore the red planet?");
   });
 
-  robot.respond(/adventure|fun|activities/i, (res) => {
-    res.send('Mars has dunes to scale, craters to jump, and plenty of mysteries to uncover. Bring your hat!');
+  robot.respond(/(?:adventure|fun|activities)/i, function (res) {
+    res.send("Mars has dunes to scale, craters to jump, and plenty of mysteries to uncover. Bring your hat!");
   });
 
-  robot.respond(/thank you|thanks/i, (res) => {
-    res.send('Anytime, adventurer. Don’t forget your whip!');
+  robot.respond(/(?:thank you|thanks)/i, function (res) {
+    res.send("Anytime, adventurer. Don’t forget your whip!");
   });
 };
