401a03fa35de6a4eaa9e118a9114126a541cc243
Shay Avraham <github@hinbit.com>
2024-12-10 13:04:38 +0200

    Update Indiana-jones-rep.js

--- scripts/Indiana-jones-rep.js
+++ scripts/Indiana-jones-rep.js
@@ -1,3 +1,12 @@
+/**
+ * Indiana Jones Rep Script
+ * This script handles greetings and fun adventure-related responses.
+ * 
+ * Commands:
+ *   hubot hey|hello|hi - Greets the user.
+ *   hubot adventure|fun|activities - Suggests Mars adventures.
+ *   hubot thank you|thanks - Responds with gratitude.
+ */
 module.exports = function (robot) {
   robot.respond(/(?:hey|hello|hi)/i, function (res) {
     res.send("Howdy, partner! This is Indy. Ready to explore the red planet?");
