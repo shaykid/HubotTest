4e573fa750d55d1ced74d99cd22760f519b59997
Shay Avraham <github@hinbit.com>
2024-12-10 11:14:34 +0200

    Create TestNvidia.js

---
+++ TestNvidia.js
@@ -0,0 +1,24 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: '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',
+  baseURL: 'https://integrate.api.nvidia.com/v1',
+})
+ 
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "meta/llama-3.3-70b-instruct",
+    messages: [{"role":"user","content":"Write a limerick about the wonders of GPU computing."}],
+    temperature: 0.2,
+    top_p: 0.7,
+    max_tokens: 1024,
+    stream: true
+  })
+   
+  for await (const chunk of completion) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || '')
+  }
+  
+}
+
+main();
